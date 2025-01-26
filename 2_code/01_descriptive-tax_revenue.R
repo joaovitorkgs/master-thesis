564bcbde0b6c33711ce58b26a11a740d30ce4856
joaovitorkgs <joao.vitor.kgs@gmail.com>
2025-01-26 17:53:03 +0100

    Federal tax revenue (code and plots)
    Updated code, created subsets and plots specific for CIDE Combustíveis tax revenue. Hard coded CNAE sectors in Portuguese and English.

--- 2_code/01_descriptive-tax_revenue.R
+++ 2_code/01_descriptive-tax_revenue.R
@@ -153,28 +153,306 @@ df_state_sc_sector <- read.csv("./1_raw_data/1_tax_revenue/2022_SC_tax_revenue_s
 sc_sectors <- unique(df_state_sc_sector$DE_SETOR_ECONOMICO)
 
 
+# 2.3. Hard-coded data ---------------------------------------------------------
+
+# Manual data on economic sectors
+
+cnae_sector_names <- data.frame(
+  secao_sigla = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U"),
+  subdivisions = c("01 .. 03", "05 .. 09", "10 .. 33", "35 .. 35", "36 .. 39", "41 .. 43", "45 .. 47", "49 .. 53", "55 .. 56", "58 .. 63", "64 .. 66", "68 .. 68", "69 .. 75", "77 .. 82", "84 .. 84", "85 .. 85", "86 .. 88", "90 .. 93", "94 .. 96", "97 .. 97", "99 .. 99"),
+  Denominacao = c(
+    "AGRICULTURA, PECUÁRIA, PRODUÇÃO FLORESTAL, PESCA E AQÜICULTURA",
+    "INDÚSTRIAS EXTRATIVAS",
+    "INDÚSTRIAS DE TRANSFORMAÇÃO",
+    "ELETRICIDADE E GÁS",
+    "ÁGUA, ESGOTO, ATIVIDADES DE GESTÃO DE RESÍDUOS E DESCONTAMINAÇÃO",
+    "CONSTRUÇÃO",
+    "COMÉRCIO; REPARAÇÃO DE VEÍCULOS AUTOMOTORES E MOTOCICLETAS",
+    "TRANSPORTE, ARMAZENAGEM E CORREIO",
+    "ALOJAMENTO E ALIMENTAÇÃO",
+    "INFORMAÇÃO E COMUNICAÇÃO",
+    "ATIVIDADES FINANCEIRAS, DE SEGUROS E SERVIÇOS RELACIONADOS",
+    "ATIVIDADES IMOBILIÁRIAS",
+    "ATIVIDADES PROFISSIONAIS, CIENTÍFICAS E TÉCNICAS",
+    "ATIVIDADES ADMINISTRATIVAS E SERVIÇOS COMPLEMENTARES",
+    "ADMINISTRAÇÃO PÚBLICA, DEFESA E SEGURIDADE SOCIAL",
+    "EDUCAÇÃO",
+    "SAÚDE HUMANA E SERVIÇOS SOCIAIS",
+    "ARTES, CULTURA, ESPORTE E RECREAÇÃO",
+    "OUTRAS ATIVIDADES DE SERVIÇOS",
+    "SERVIÇOS DOMÉSTICOS",
+    "ORGANISMOS INTERNACIONAIS E OUTRAS INSTITUIÇÕES EXTRATERRITORIAIS"),
+    sectors_en = c(
+      "Agriculture",
+      "Extractive industries",
+      "Manufacturing industries",
+      "Electricity and gas",
+      "Water and waste management",
+      "Construction",
+      "Trade; repair of vehicles",
+      "Transport, storage and mail",
+      "Accommodation and food",
+      "Information and communication",
+      "Financial and insurance services",
+      "Real estate activities",
+      "Scientific and technical activities",
+      "Administrative activities",
+      "Public administration",
+      "Education",
+      "Human health and social services",
+      "Arts, culture, sports & recreation",
+      "Other service activities",
+      "Domestic services",
+      "International organizationss"
+    )) %>% 
+  mutate(sectors_en = paste0(secao_sigla, sep = " - ", sectors_en))
+
+
+
 
 # 3. Tax Revenue analysis ------------------------------------------------------
 
-federal_energy <- df_federal_cnae %>% 
-  select(ano,mes,secao_sigla,cide_combustiveis) %>% 
-  filter(secao_sigla != "D") %>% 
-  group_by(ano) %>% 
-  summarise(
-    total = sum(cide_combustiveis))
+# 3.1. Federal Tax Revenue -----------------------------------------------------
 
-federal_fuel <- df_federal_cnae %>% 
-  select(ano,mes,secao_sigla,cide_combustiveis) %>% 
-  mutate(cide_combustiveis = as.numeric(cide_combustiveis)) %>% 
-  group_by(ano) %>% 
-  summarise(
-    total = sum(cide_combustiveis))
+# 3.1.1 Data on tax revenue per legal nature of registered firms ---------------
+unique(df_federal_natjuridica$natureza_juridica_codigo_descricao)
 
-# Noch zu tun: figure out why the grouping function is not working; exploring the
-# data per tax type and plot the trendlines over years; 
+# Finding: I cannot distinguish the economic activity or anything related to fuel
+# based on this data frame.
 
-# Also: look into the literature to see what the tax composition of fuels looks like
-# for gasoline
+
+# 3.1.2 Data on tax revenue per economic sector of registered firms ------------
+
+# Cleaning the original data set 
+
+df_federal_cnae_date <- df_federal_cnae %>% 
+  mutate(
+    mes = as.numeric(as.character(mes)),    # Convert 'mes' to numeric if it's not
+    mes = sprintf("%02d", mes),             # Format 'mes' as double digits
+    ano_mes = paste(ano, mes, sep = "-"),   # Combine 'ano' and 'mes'
+    ano_mes_date = as.Date(paste(ano, mes, "01", sep = "-")) # Create date column
+  ) 
+
+
+# Dataframes focusing on revenue from CIDE Combustivel only
+
+federal_gasstation <- df_federal_cnae_date %>% 
+  select(ano_mes_date, ano, mes, secao_sigla, cide_combustiveis) %>%  # "cide_combustiveis" refers to a specific tax paid for fuel
+  filter(secao_sigla != "G") %>%  # Removing all registered companies not in the energy sector
+  mutate(cide_combustiveis = as.numeric(as.character(cide_combustiveis))) %>% 
+  group_by(ano_mes_date) %>% 
+  summarise(`CIDE Combustíveis` = sum(cide_combustiveis, na.rm = TRUE))
+
+federal_energysector <- df_federal_cnae_date %>% 
+  select(ano_mes_date, ano, mes, secao_sigla, cide_combustiveis) %>%  # "cide_combustiveis" refers to a specific tax paid for fuel
+  filter(secao_sigla != "D") %>%  # Removing all registered companies not in the energy sector
+  mutate(cide_combustiveis = as.numeric(as.character(cide_combustiveis))) %>% 
+  group_by(ano_mes_date) %>% 
+  summarise(`CIDE Combustíveis` = sum(cide_combustiveis, na.rm = TRUE))
+
+federal_fuel <- df_federal_cnae_date %>% 
+  select(ano_mes_date, ano, mes, secao_sigla, cide_combustiveis) %>%  # "cide_combustiveis" refers to a specific tax paid for fuel
+  mutate(cide_combustiveis = as.numeric(as.character(cide_combustiveis))) %>% 
+  group_by(ano_mes_date, secao_sigla) %>% 
+  summarise(`CIDE Combustíveis` = sum(cide_combustiveis, na.rm = TRUE))
+
+federal_fuel <- federal_fuel %>% 
+  left_join(cnae_sector_names, by = "secao_sigla") %>% 
+  select(ano_mes_date, secao_sigla, sectors_en, `CIDE Combustíveis`) %>% 
+  drop_na()
+
+
+
+
+# 4. Plots ---------------------------------------------------------------------
+
+# 4.1. Federal Tax Revenue -----------------------------------------------------
+
+# 4.1.1. CIDE Combustiveis -----------------------------------------------------
+
+# Economic Sector D (Electricity and Gas)
+
+plot_trend_fed_revenue_cnae_electgas <- ggplot(federal_energysector, 
+                                                 aes(x = ano_mes_date, 
+                                                     y = (`CIDE Combustíveis`)/1000000000)) +
+  geom_line(linewidth = 1) +  # Line plot for each 'tipo_consumo'
+  theme_bw() +                  # Clean theme
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +  # Show only the first month of each year
+  scale_y_continuous(labels = scales::comma) +               # Format y-axis labels
+  labs(
+    x = "Year",
+    y = "Total Tax Revenue (Billion BRL)",
+    title = "Total Monthly Federal Tax Revenue from CIDE Combustíveis",
+    subtitle = "Source: Special Secretariat of the Federal Revenue of Brazil (RFB), 2024",
+    caption = "Note: Data related to firms registered under D category only, which refers to the electricity and gas sector."
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Tilt x-axis labels
+    plot.caption = element_text(hjust = 0))
+
+ggsave("./4_plots/plot_trend_fed_revenue_cnae_electgas.png",
+       plot = plot_trend_fed_revenue_cnae_electgas,
+       units = "in")
+    
+
+# Economic Sector G (includes gas stations)
+
+plot_trend_fed_revenue_cnae_gasstation <- ggplot(federal_gasstation, 
+                                      aes(x = ano_mes_date, 
+                                          y = (`CIDE Combustíveis`)/1000000000)) +
+  geom_line(linewidth = 1) +  # Line plot for each 'tipo_consumo'
+  theme_bw() +                  # Clean theme
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +  # Show only the first month of each year
+  scale_y_continuous(labels = scales::comma) +               # Format y-axis labels
+  labs(
+    x = "Year",
+    y = "Total Tax Revenue (Billion BRL)",
+    title = "Total Monthly Federal Tax Revenue from CIDE Combustíveis",
+    subtitle = "Source: Special Secretariat of the Federal Revenue of Brazil (RFB), 2024",
+    caption = "Note: Data related to firms registered under G category only, which includes gas stations and retail sale of fuels."
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Tilt x-axis labels
+    plot.caption = element_text(hjust = 0)
+                               
+  )
+
+ggsave("./4_plots/plot_trend_fed_revenue_cnae_gasstation.png",
+       plot = plot_trend_fed_revenue_cnae_gasstation,
+       units = "in")
+
+
+# All sectors
+
+plot_trend_fed_revenue_cnae_groups <- ggplot(federal_fuel,
+                                             aes(x = ano_mes_date, y = `CIDE Combustíveis`)) +
+    geom_line(linewidth = 0.7) +
+  theme_bw() +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  scale_y_continuous(trans = "log10",
+                     breaks = c(1e5, 1e6, 1e7, 1e8, 1e9, 1e10),
+                     labels = c("100k", "1M", "10M", "100M", "1B", "10B")) +
+  labs(
+    x = "Year",
+    y = "Total Tax Revenue (BRL) - log transformed",
+    title = "Total Monthly Federal Tax Revenue from CIDE Combustíveis",
+    subtitle = "Source: Special Secretariat of the Federal Revenue of Brazil (RFB), 2024"
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  ) +
+  facet_wrap(~ sectors_en, nrow = 6)
+
+ggsave("./4_plots/plot_trend_fed_revenue_cnae_groups.png",
+       plot = plot_trend_fed_revenue_cnae_groups,
+       units = "in",
+       width = 10,
+       height = 10)
+
+# Note: there seems to be an issue with the data, as the revenue from 2024 grows
+# exponentially for some sectors. It is either a problem with data input, or there
+# is a change on the legislation for this tax specifically which I am not aware of
+
+
+
+# 4.1.1. CIDE Combustiveis (up to 2023 only) -----------------------------------
+
+
+# Changing the data sets to remove values after January 2024 
+
+federal_energysector_23 <-  federal_energysector %>% 
+  filter(ano_mes_date < as.Date(paste("2024", "01", "01", sep = "-")))
+
+federal_gasstation_23 <- federal_gasstation %>% 
+  filter(ano_mes_date < as.Date(paste("2024", "01", "01", sep = "-")))
+
+federal_fuel_23 <- federal_fuel %>% 
+  filter(ano_mes_date < as.Date(paste("2024", "01", "01", sep = "-")))
+  
+
+
+# Economic Sector D (Electricity and Gas) up to December 2023
+
+plot_trend_fed_revenue_cnae_electgas_23 <- ggplot(federal_energysector_23, 
+                                               aes(x = ano_mes_date, 
+                                                   y = (`CIDE Combustíveis`)/1000000000)) +
+  geom_line(linewidth = 1) +  # Line plot for each 'tipo_consumo'
+  theme_bw() +                  # Clean theme
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +  # Show only the first month of each year
+  scale_y_continuous(labels = scales::comma) +               # Format y-axis labels
+  labs(
+    x = "Year",
+    y = "Total Tax Revenue (Billion BRL)",
+    title = "Total Monthly Federal Tax Revenue from CIDE Combustíveis up to December 2023",
+    subtitle = "Source: Special Secretariat of the Federal Revenue of Brazil (RFB), 2024",
+    caption = "Note: Data related to firms registered under D category only, which refers to the electricity and gas sector."
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Tilt x-axis labels
+    plot.caption = element_text(hjust = 0))
+
+ggsave("./4_plots/plot_trend_fed_revenue_cnae_electgas_23.png",
+       plot = plot_trend_fed_revenue_cnae_electgas_23,
+       units = "in")
+
+
+# Economic Sector G (includes gas stations) up to December 2023
+
+plot_trend_fed_revenue_cnae_gasstation_23 <- ggplot(federal_gasstation_23, 
+                                                 aes(x = ano_mes_date, 
+                                                     y = (`CIDE Combustíveis`)/1000000000)) +
+  geom_line(linewidth = 1) +  # Line plot for each 'tipo_consumo'
+  theme_bw() +                  # Clean theme
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +  # Show only the first month of each year
+  scale_y_continuous(labels = scales::comma) +               # Format y-axis labels
+  labs(
+    x = "Year",
+    y = "Total Tax Revenue (Billion BRL)",
+    title = "Total Monthly Federal Tax Revenue from CIDE Combustíveis up to December 2023",
+    subtitle = "Source: Special Secretariat of the Federal Revenue of Brazil (RFB), 2024",
+    caption = "Note: Data related to firms registered under G category only, which includes gas stations and retail sale of fuels."
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Tilt x-axis labels
+    plot.caption = element_text(hjust = 0)
+    
+  )
+
+ggsave("./4_plots/plot_trend_fed_revenue_cnae_gasstation_23.png",
+       plot = plot_trend_fed_revenue_cnae_gasstation_23,
+       units = "in")
+
+
+# All sectors up to December 2023
+
+plot_trend_fed_revenue_cnae_groups_23 <- ggplot(federal_fuel_23,
+                                             aes(x = ano_mes_date, y = `CIDE Combustíveis`)) +
+  geom_line(linewidth = 0.7) +
+  theme_bw() +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  scale_y_continuous(trans = "log10",
+                     breaks = c(1e5, 1e6, 1e7, 1e8, 1e9, 1e10),
+                     labels = c("100k", "1M", "10M", "100M", "1B", "10B")) +
+  labs(
+    x = "Year",
+    y = "Total Tax Revenue (BRL) - log transformed",
+    title = "Total Monthly Federal Tax Revenue from CIDE Combustíveis up to December 2023",
+    subtitle = "Source: Special Secretariat of the Federal Revenue of Brazil (RFB), 2024"
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  ) +
+  facet_wrap(~ sectors_en, nrow = 6)
+
+ggsave("./4_plots/plot_trend_fed_revenue_cnae_groups_23.png",
+       plot = plot_trend_fed_revenue_cnae_groups_23,
+       units = "in",
+       width = 10,
+       height = 10)
+
+# Note: now the data is less distorted for the two specific sectors (D and G), but
+# the plots suggest that the tax revenue for these two industries are highly correlated.
+
 
 
 

fd9d12f8ba95f5a6788438c67e0dd0d431a4a597
joaovitorkgs <joao.vitor.kgs@gmail.com>
2025-02-22 16:43:28 +0100

    Linear model interpretation and MARS

--- 2_code/00_packages.R
+++ 2_code/00_packages.R
@@ -4,12 +4,14 @@ pacman::p_load(
   # Core Data Manipulation and Tidying
   dplyr,        # Data manipulation: filtering, summarizing, mutating, and more
   tidyr,        # Data tidying and reshaping: pivoting, handling missing values
+  janitor,      # Clean messy data (e.g., column names) and create summary tables
   
-  # Data Import
+  # Data Import and Export
   readr,        # Read flat files (CSV, TSV) into tibbles
   readxl,       # Read Excel files (.xls, .xlsx) into data frames
   basedosdados, # Access Brazilian public data via BigQuery
   httr,         # Perform HTTP requests to interact with web APIs
+  archive,      # Extract files from compressed archives (.zip, .tar.gz)
   
   # Data Visualization
   ggplot2,      # Create customizable data visualizations using the grammar of graphics
@@ -32,20 +34,21 @@ pacman::p_load(
   gt,           # Tables for reporting results
   stargazer,    # Generate regression tables in LaTeX/HTML/text formats
   
-  # Machine Learning and Metrics
+  # Machine Learning, Statistics, and Metrics
   caret,        # Comprehensive machine learning framework (classification/regression)
   Metrics,      # Evaluate model performance (e.g., RMSE, MAE)
-  
-  # Utilities
-  janitor,      # Clean messy data (e.g., column names) and create summary tables
-  archive,      # Extract files from compressed archives (.zip, .tar.gz)
+  glmnet,       # Regularized generalized linear models (Lasso, Ridge)
+  earth,        # Multivariate Adaptive Regression Splines
+  vip,          # Variable Importance Plots
+  pdp,          # Partial Dependence Plots
+  car,          # Companion to Applied Regression (diagnostic functions)
+  DescTools,    # Tools for descriptive statistics
   
   # Specialized Packages
   plm,          # Perform panel data analysis (fixed/random effects models)
-  deflateBr,    # Deflate nominal Brazilian Reais using price indexes
-  car,
-  glmnet,
-  DescTools
+  deflateBr,     # Deflate nominal Brazilian Reais using price indexes
+  
+  rsample
 )
 
 
--- 2_code/03_models.R
+++ 2_code/03_models.R
@@ -32,9 +32,6 @@ fleet_df <- fleet_2013_2024_clean %>%
   filter(log_electric >= 0) %>% 
   left_join(months, by="date")
 
-summary(fleet_df)
-options(scipen = 999)
-
 ### b) Price information -------------------------------------------------------
 
 fleet_df_b_city <- fleet_df %>% 
@@ -53,7 +50,7 @@ fleet_df_b_uf <- fleet_df %>%
 
 fleet_df_b_uf <- fleet_df %>% 
   left_join(price_df, by = "date") %>% 
-  group_by(sigla_uf, date, year, month) %>% 
+  group_by(sigla_uf, date, year, month, months_nr) %>% 
   summarize(diesel     = sum(diesel),
             ethanol    = sum(ethanol),
             gasoline   = sum(gasoline),
@@ -70,9 +67,7 @@ fleet_df_b_uf <- fleet_df %>%
             min_gas    = mean(min_price_gasoline),
             min_hyb    = mean(min_price_hybrid),
             min_ev     = mean(min_price_electric)) %>% 
-  mutate(ev_pc        = electric / population,
-         ff_pc        = (gasoline+diesel)/population,
-         log_electric = log(electric),
+  mutate(ff_pc        = (gasoline+diesel)/population,
          log_ff       = log(gasoline+diesel),
          log_pop      = log(population))
 
@@ -107,7 +102,8 @@ stargazer(model2, type = "html",
           out = "./5_analysis/1_regression_tables/lm_model2.html")
 
 
-model3 <- lm(log_electric ~ log_pop * months_nr + mean_ev + mean_gas + mean_hyb, data = fleet_df_b_uf)
+model3 <- lm(log_electric ~ log_pop * months_nr + mean_ev + mean_gas + mean_hyb,
+             data = fleet_df_b_uf)
 
 stargazer(model3, type = "text",
        title="Linear Regression Results: Model 3", single.row=TRUE)
@@ -128,20 +124,7 @@ fe_model_2 <- plm(electric ~ log(populacao) + log(gasoline) + log(diesel) + log(
                   model = "within")
 
 
-panel_data_b <- pdata.frame(fleet_df_b_uf, index = c("sigla_uf", "date"))
-
-fe_model_3 <- plm(electric ~ log(population) + log(gasoline) + log(diesel) + log(ethanol) + mean_ev + mean_gas + mean_hyb + as.factor(months_nr),
-                  data  = panel_data_b,
-                  model = "within",
-                  singular.ok = TRUE)
-
-stargazer(fe_model_1, fe_model_2, fe_model_3, type = "text")
-
-stargazer(fe_model_1, fe_model_2, fe_model_3, type = "html",
-          title="Fixed-Effects Linear Regression Results: Panel Data", single.row=TRUE,
-          out = "./5_analysis/1_regression_tables/fe_models_1&2.html")
-
-vif(fe_model_3)
+stargazer(fe_model_1, fe_model_2, type = "text")
 
 
 ### 3.1.3 Panel data with fixed effects for states -----------------------------
@@ -161,8 +144,6 @@ stargazer(fe_model_1, fe_model_2, type = "text")
 stargazer(fe_model_1, fe_model_2, type = "html",
           title="Fixed-Effects Linear Regression Results: Panel Data", single.row=TRUE,
           out = "./5_analysis/1_regression_tables/fe_models_1&2.html")
-
-
 
 
 ### 3.1.3. Time Series ---------------------------------------------------------
@@ -206,8 +187,10 @@ plot(forecast_result,
 # Create training  feature matrices
 # we use model.matrix(...)[, -1] to discard the intercept
 
-X <- model.matrix(electric ~ ., fleet_df_b_uf)[, -1]
-Y <- log(fleet_df_b_uf$electric)
+library(glmnet)
+
+X <- modglmnetX <- model.matrix(electric ~ ., fleet_df_c)[, -1]
+Y <- log(fleet_df_c$electric)
 
 # Ridge model
 ridge_min <- glmnet(
@@ -262,8 +245,171 @@ pred <- predict(cv_glmnet, X)
 RMSE(exp(pred), exp(Y))
 
 # The first conclusion here is that this first model using just population data
-# predicts a RMSE of 1550 given the observations (nr of evs in # all 25 Brazilian
-# UFs in the past 11 years. )
+# predicts a RMSE (Root Mean Square Error) of 372. 
+
+# This means that, on average, my model}s predictions deviate
+# from the actual number of electric vehicles by about 372 vehicles.
+
+fleet_df_explore <- fleet_df_c %>% 
+  group_by(year, sigla_uf) %>% 
+  summarize(electric = mean(electric))
+
+
+ggplot(fleet_df_explore, aes(x=electric)) +
+  geom_histogram() +
+  ggtitle("Distribution of Electric Vehicles in Brazil")+
+  scale_fill_viridis(discrete=TRUE) +
+  scale_color_viridis(discrete=TRUE) +
+  theme_bw() +
+  facet_wrap(~year, nrow = 3) +
+  ylab("Density") +
+  xlab("Amount of EVs in Brazil per Year") 
+
+# Considering the data distribution, this is a large value for the RSME. This
+# model is still performing poorly.
+
+
+
+
+
+
+## 3.3. Multivariate Adaptive Regression Splines--------------------------------
+
+### Models without interaction -------------------------------------------------
+
+# Creating the MARS model object
+mars1 <- earth(
+  log(electric) ~ .,
+  data = fleet_df_c
+)
+
+print(mars1)
+summary(mars1)
+
+# Ploting the model selection
+plot(mars1, which = 1)
+
+# Interpretation of the plot: the vertical dashed lined at 25 tells us the optimal 
+# number of terms retained where marginal increases in GCV (generalized cross-validation)
+# R²  are less than 0.001.
+
+# For this model, 25 non-intercept terms were retained based on the 29 predictors.
+
+### Models with interaction ----------------------------------------------------
+
+# Creating the model object including non.linear relationships and polynomial terms
+mars2 <- earth(
+  log(electric) ~ .,
+  data = fleet_df_c,
+  degree = 2
+)
+
+# Summary of the most important predictors and interactions
+summary(mars2) %>% .$coefficients %>% head(10)
+
+# Preparing a grid with hyperparameters
+hyper_grid <- expand.grid(
+  degree = 1:3, 
+  nprune = seq(2, 100, length.out = 10) %>% floor()
+)
+# Explanation: hyperparameters minimize prediction error by choosing how many terms
+# in the final model and the complexity of the interactions. Grid search provides
+# a set of different combinations of these two combinations. This is intended to
+# prevent overfitting (the model does well for training data, but not well for testing
+# or validating data) and underfitting (where the model is too simple to capture relevant
+# patterns.)
+
+
+# Training the algorithm 
+set.seed(123)  # for reproducibility
+cv_mars <- train(
+  x = subset(fleet_df_c, select = -electric),
+  y = fleet_df_c$electric,
+  method = "earth",
+  metric = "RMSE",
+  trControl = trainControl(method = "cv", number = 10),
+  tuneGrid = hyper_grid
+)
+
+# Finding the best hyperparameters
+cv_mars$bestTune
+cv_mars$results %>%
+  filter(nprune == cv_mars$bestTune$nprune, degree == cv_mars$bestTune$degree)
+
+# Plotting the RMSE according to the number of terms
+ggplot(cv_mars)
+
+
+# Mapping out the most relevant predictors
+p1 <- vip(cv_mars, num_features = 25, geom = "point", value = "gcv") + ggtitle("GCV")
+p2 <- vip(cv_mars, num_features = 25, geom = "point", value = "rss") + ggtitle("RSS")
+
+# Population and log_pop are coming up as the most important predictors
+# Maybe I should use a different predictor instead?
+
+
+gridExtra::grid.arrange(p1, p2, ncol = 2)
+
+# Check for interactions
+cv_mars$finalModel %>%
+  coef() %>%  
+  broom::tidy() %>%  
+  filter(stringr::str_detect(names, "\\*")) 
+# So far, the model is not yielding any relevant variables
+
+
+
+cv_mars$finalModel %>%
+  coef() %>%  
+  broom::tidy() %>%  
+  filter(stringr::str_detect(names, "\\*")) 
+
+# Construct partial dependence plots
+p1 <- partial(cv_mars, pred.var = "population", grid.resolution = 10) %>% 
+  autoplot()
+p2 <- partial(cv_mars, pred.var = "avg_taxable_income_100", grid.resolution = 10) %>% 
+  autoplot()
+p3 <- partial(cv_mars, pred.var = c("population", "avg_taxable_income_100"), 
+              grid.resolution = 10) %>% 
+  plotPartial(levelplot = FALSE, zlab = "yhat", drape = TRUE, colorkey = TRUE, 
+              screen = list(z = -20, x = -60))
+
+# Display plots side by side
+gridExtra::grid.arrange(p1, p2, p3, ncol = 3)
+
+
+?
+# get attrition data
+df <- rsample::attrition %>% mutate_if(is.ordered, factor, ordered = FALSE)
+
+# Create training (70%) and test (30%) sets for the rsample::attrition data.
+# Use set.seed for reproducibility
+set.seed(123)
+churn_split <- rsample::initial_split(df, prop = .7, strata = "Attrition")
+churn_train <- rsample::training(churn_split)
+churn_test  <- rsample::testing(churn_split)
+
+
+# for reproducibiity
+set.seed(123)
+
+# cross validated model
+tuned_mars <- train(
+  x = subset(churn_train, select = -Attrition),
+  y = churn_train$Attrition,
+  method = "earth",
+  trControl = trainControl(method = "cv", number = 10),
+  tuneGrid = hyper_grid
+)
+
+# best model
+tuned_mars$bestTune
+##   nprune degree
+## 2     12      1
+
+# plot results
+ggplot(tuned_mars)
+
 
 
 ## 4. Projections --------------------------------------------------------------
